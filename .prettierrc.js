50b00e8ab22577a6b49b9c46c9a8d6467b781648
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-08-01 01:30:05 -0300

    chore(prettier): update print width and change doble quotes to single one

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-  printWidth: 120,
+  printWidth: 80,
   bracketSpacing: true,
-  arrowParens: "always",
+  arrowParens: 'always',
 }
