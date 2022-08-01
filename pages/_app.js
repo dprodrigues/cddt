6c7dc0c9432bb6c7cc7e4dc9c7f87993dd1349bc
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-08-01 01:34:42 -0300

    feat(_app): add file

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import { FirebaseProvider } from '@/contexts/firebase'
+import '@/styles/global.css'
+
+const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+    <FirebaseProvider>
+      {getLayout(<Component {...pageProps} />)}
+    </FirebaseProvider>
+  )
+}
+
+export default MyApp
