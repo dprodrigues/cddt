684d66a4ddba299054d9f27674cf75974dddf06a
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-08-02 23:29:00 -0300

    feat(user): add hook to provide user informations

---
+++ hooks/user.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useFirebase } from '@/contexts/firebase'
+
+export const useUser = () => {
+  const [user, setUser] = useState()
+  const [loading, setLoading] = useState(true)
+  const { auth } = useFirebase()
+
+  useEffect(() => {
+    if (!auth) {
+      return
+    }
+
+    const unsubscriber = onAuthStateChanged(auth, async (user) => {
+      try {
+        if (user) {
+          const { uid, displayName, email, photoURL } = user
+          setUser({ uid, displayName, email, photoURL })
+        } else setUser(null)
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setLoading(false)
+      }
+    })
+    return () => unsubscriber()
+  }, [auth])
+
+  return { user, loading }
+}
