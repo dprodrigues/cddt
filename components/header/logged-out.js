bbb88c75379d8abd6e510d0e8babc1989a7ed000
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-08-02 23:28:33 -0300

    feat(header): add logged out component

---
+++ components/header/logged-out.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export const LoggedOut = () => (
+  <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+    <Link href="/sign-in">
+      <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+        Sign in
+      </a>
+    </Link>
+
+    <Link href="/sign-up">
+      <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-rose-800 hover:bg-rose-700 active:bg-rose-900">
+        Sign up
+      </a>
+    </Link>
+  </div>
+)
