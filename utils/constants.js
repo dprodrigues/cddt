656d022375c93808ec763c73804fe39e37be3f13
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-08-03 22:54:11 -0300

    feat(utils): add constant file with email and password regex

---
+++ utils/constants.js
@@ -0,0 +1,4 @@
+export const REGEX_EMAIL =
+  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+export const REGEX_PASSWORD =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
