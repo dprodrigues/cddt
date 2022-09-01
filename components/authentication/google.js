f71b671cb0c6f90366770dc1d9fa5e0be8c7a4d1
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-08-31 22:23:07 -0300

    refactor(auth): change google button structure

--- components/authentication/google.js
+++ components/authentication/google.js
@@ -1,22 +1,15 @@
-import { signInWithPopup } from 'firebase/auth'
 import { FaGoogle } from 'react-icons/fa'
 import { useFirebase } from '@/contexts/firebase'
 import { SocialButton } from './social-button'
 
 export const GoogleButton = () => {
-  const { auth, provider } = useFirebase()
-
-  const handleClick = async () => {
-    try {
-      await signInWithPopup(auth, provider.google)
-    } catch (error) {
-      console.error('error:', error)
-    }
-  }
+  const { provider } = useFirebase()
 
   return (
-    <SocialButton onClick={handleClick}>
-      <FaGoogle />
-    </SocialButton>
+    <SocialButton
+      label="Google"
+      icon={<FaGoogle />}
+      provider={provider.google}
+    />
   )
 }
