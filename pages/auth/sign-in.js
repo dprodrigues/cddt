7fd88f14e455670b43060cf5290937bc1080ea97
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-08-31 22:26:41 -0300

    refactor(auth): change sign in page structure to only support social media login

--- pages/auth/sign-in.js
+++ pages/auth/sign-in.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { LayoutBasic } from '@/components/layout/basic'
-import { SignInForm, SocialMediaButtons } from '@/components/authentication'
+import { SocialMediaButtons } from '@/components/authentication'
+import { LayoutAuth } from '@/components/layout'
 import { useUser } from '@/hooks/user'
 
 const SignIn = () => {
@@ -14,29 +14,11 @@ const SignIn = () => {
     }
   }, [user?.email, router])
 
-  return (
-    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
-
-        <div className="space-y-6 bg-white border-gray-200 border rounded-lg p-8">
-          <SignInForm />
-
-          <div className="text-sm flex justify-center text-gray-400">
-            Or continue with
-          </div>
-
-          <SocialMediaButtons />
-        </div>
-      </div>
-    </div>
-  )
+  return <SocialMediaButtons />
 }
 
-SignIn.getLayout = (page) => <LayoutBasic>{page}</LayoutBasic>
+SignIn.getLayout = (page) => (
+  <LayoutAuth title="Sign in with social media">{page}</LayoutAuth>
+)
 
 export default SignIn
