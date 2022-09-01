32a5fa104cb3ec0e3e2ab2a4b533b23efccd25a3
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-08-31 22:26:04 -0300

    refactor(auth): add github and twitter button and add tailwind classes

--- components/authentication/social-buttons.js
+++ components/authentication/social-buttons.js
@@ -1,7 +1,13 @@
 import { GoogleButton } from './google'
+import { GithubButton } from './github'
+import { TwitterButton } from './twitter'
 
 export const SocialMediaButtons = () => (
-  <div className="flex space-x-4">
+  <div className="flex w-full flex-col space-y-4">
     <GoogleButton />
+
+    <GithubButton />
+
+    <TwitterButton />
   </div>
 )
