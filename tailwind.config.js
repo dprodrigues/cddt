6e13989b93a18954bb60413eedc8957cb60f88d9
Daniel Rodrigues <danielprodrigues0@gmail.com>
2023-10-24 23:01:32 -0300

    chore: add all breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
       '2xl': '1536px',
     },
   },
