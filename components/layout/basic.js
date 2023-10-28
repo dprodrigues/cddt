291f39ba02851b26c5544eeb3c58afece11b17dc
Daniel Rodrigues <danielprodrigues0@gmail.com>
2023-10-28 13:34:04 -0300

    refactor: remove unused layout files

--- components/layout/basic.js
+++
@@ -1,3 +0,0 @@
-export const LayoutBasic = ({ children }) => {
-  return <>{children}</>
-}
--- components/layout/index.js
+++
@@ -1,4 +0,0 @@
-'use client'
-export default function MainLayout({ children }) {
-  return <>{children}</>
-}
