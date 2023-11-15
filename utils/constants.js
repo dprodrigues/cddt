16dbd919dddae8db61b65ad48d58e104383ca464
Daniel Rodrigues <danielprodrigues0@gmail.com>
2023-11-14 23:10:15 -0300

    refactor: remove unused code

--- utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const REGEX_EMAIL =
-  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-export const REGEX_PASSWORD =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
