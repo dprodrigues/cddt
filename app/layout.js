9d3681e9cdf22d725ecc1454a3990b18cca20c38
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-01-06 19:43:22 -0300

    refactor: change layout viewport and adjust import

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { ServerAuthProvider } from '@/auth/server-auth-provider'
+import { ServerAuthProvider } from '@/contexts/auth/server-provider'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import '@/styles/global.css'
@@ -6,7 +6,11 @@ import '@/styles/global.css'
 export const metadata = {
   title: 'cddt',
   description: 'ToDo APP',
-  viewport: 'width=device-width, initial-scale=1',
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
 }
 
 export default function RootLayout({ children }) {
