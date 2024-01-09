d6bd99a3987488998fb36a9c8230a72c216370bf
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-01-08 21:12:35 -0300

    refactor(home): add header and footer to home page directly

--- app/page.js
+++ app/page.js
@@ -1,44 +1,49 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Container } from '@/components/container'
+import Header from '@/components/header'
 
 export default function Home() {
   return (
-    <Container>
-      <div>
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-gray-900 text-center md:text-left">
-          Organize Your Life, One Note at a Time.
-        </h1>
+    <>
+      <Header />
 
-        <p className="mt-6 text-lg leading-8 text-gray-600 text-center md:text-left">
-          Empower Your Productivity: Your ultimate task manager and note-taking
-          app for enhanced focus and organization.
-        </p>
+      <Container>
+        <div>
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-gray-900 text-center md:text-left">
+            Organize Your Life, One Note at a Time.
+          </h1>
 
-        <div className="mt-10 flex items-center justify-center md:justify-start gap-x-6">
-          <Link
-            href="/auth/register"
-            className="rounded-md bg-violet-800 hover:bg-violet-700 active:bg-violet-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-          >
-            Get started for free
-          </Link>
+          <p className="mt-6 text-lg leading-8 text-gray-600 text-center md:text-left">
+            Empower Your Productivity: Your ultimate task manager and
+            note-taking app for enhanced focus and organization.
+          </p>
 
-          <Link
-            href="/about"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Learn more <span aria-hidden="true">→</span>
-          </Link>
+          <div className="mt-10 flex items-center justify-center md:justify-start gap-x-6">
+            <Link
+              href="/auth/register"
+              className="rounded-md bg-violet-800 hover:bg-violet-700 active:bg-violet-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+            >
+              Get started for free
+            </Link>
+
+            <Link
+              href="/about"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Learn more <span aria-hidden="true">→</span>
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <Image
-        className="w-auto max-w-md lg:max-w-lg lg:-mr-8 z-0"
-        src="/svgs/home-art.svg"
-        alt="logo"
-        height={500}
-        width={500}
-      />
-    </Container>
+        <Image
+          className="w-auto max-w-md lg:max-w-lg lg:-mr-8 z-0"
+          src="/svgs/home-art.svg"
+          alt="logo"
+          height={500}
+          width={500}
+        />
+      </Container>
+    </>
   )
 }
