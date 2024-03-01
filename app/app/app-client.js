21a35510a9e12bd86387367ab7dae43a8baba97e
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-03-01 08:48:32 -0300

    refactor(app-home): render the selected project title

--- app/app/app-client.js
+++ app/app/app-client.js
@@ -2,12 +2,19 @@
 
 import { Box, Flex, Heading } from '@radix-ui/themes';
 import Greetings from '@/components/greetings';
+import { useProjects } from '@/contexts/projects';
 
 export default function AppClient() {
+  const {
+    state: { selected },
+  } = useProjects();
+
   return (
     <Box px="4" py="6">
       <Flex direction="column">
         <Greetings />
+
+        {selected && <Heading>{selected.title}</Heading>}
       </Flex>
     </Box>
   );
